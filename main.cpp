4894885d250727c901c2ba639bf48d7b40bdecc8
JackBG <mabgrivol@gmail.com>
2019-08-18 18:27:11 -0300

    passando funcao por referencia

--- main.cpp
+++ main.cpp
@@ -4,66 +4,66 @@
 using namespace std;
 
 
-void interpretadorDeComandos (Comandos *comando) {
-    if (comando->keyword == "CT") {
-        cin >> comando->keyword;
-        comando->criarArquivoComNomeTabela();
+void interpretadorDeComandos (Comandos &comando) {
+    if (comando.keyword == "CT") {
+        cin >> comando.keyword;
+        comando.criarArquivoComNomeTabela();
     }
 
-    else if (comando->keyword == "RT") {
-        cin >> comando->keyword;
-        comando->apagaArquivoComNomeTabela();
+    else if (comando.keyword == "RT") {
+        cin >> comando.keyword;
+        comando.apagaArquivoComNomeTabela();
     }
 
-    else if (comando->keyword == "AT") {
+    else if (comando.keyword == "AT") {
 
     }
 
-    else if (comando->keyword == "LT") {
+    else if (comando.keyword == "LT") {
         
     }
 
-    else if (comando->keyword == "IR") {
+    else if (comando.keyword == "IR") {
 
     }
 
-    else if (comando->keyword == "BR") {
-        cin >> comando->keyword;
-        if (comando->keyword == "N") {
+    else if (comando.keyword == "BR") {
+        cin >> comando.keyword;
+        if (comando.keyword == "N") {
 
         }
-        else if (comando->keyword == "U") {
+        else if (comando.keyword == "U") {
 
         }
     }
 
-    else if (comando->keyword == "AR") {
+    else if (comando.keyword == "AR") {
 
     }
 
-    else if (comando->keyword == "RR") {
+    else if (comando.keyword == "RR") {
 
     }
 
-    else if (comando->keyword == "CI") {
-        cin >> comando->keyword;
-        if (comando->keyword == "A") {
+    else if (comando.keyword == "CI") {
+        cin >> comando.keyword;
+        if (comando.keyword == "A") {
 
         }
-        else if (comando->keyword == "H") {
+        else if (comando.keyword == "H") {
             
         }
     }
 
-    else if (comando->keyword == "RI") {
+    else if (comando.keyword == "RI") {
 
     }
 
-    else if (comando->keyword == "GI") {
+    else if (comando.keyword == "GI") {
 
     }
 
-    else if (comando->keyword == "EB") {
+    else if (comando.keyword == "EB") {
         return;
     }
 }
@@ -72,7 +72,7 @@ int main() {
     Comandos comando;
     while (comando.keyword != "EB") {
         cin >> comando.keyword;
-        interpretadorDeComandos(&comando);
+        interpretadorDeComandos(comando);
     }
 
     return 0;
