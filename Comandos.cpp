5c66fa66cc5d60c20e90c4c0093dbf468c95e6d6
vccolombo <victorcora98@gmail.com>
2019-08-18 21:13:09 -0300

    Adicionar todos os comandos da etapa 1
    Os comandos passaram a receber os valores como argumento ao invés de atualizar um parâmetro da classe Comandos. Isso facilita questões como evitar leituras de strings sem querer, e encapsulamento.

--- Comandos.cpp
+++ Comandos.cpp
@@ -1,13 +1,29 @@
 #include "Comandos.h"
 
-Comandos::Comandos() {
+Comandos::Comandos() {}
 
+void Comandos::criarArquivoComNomeTabela(string _tabela, string _campos) {
+  cout << "Criando tabela com nome: " << _tabela << "\n";
 }
 
-void Comandos::criarArquivoComNomeTabela(string campos) {
-    cout << "Criando tabela com nome: " << Comandos::keyword << "\n";
+void Comandos::apagaArquivoComNomeTabela(string _tabela) {
+  cout << "Apagando tabela " << _tabela << "\n";
 }
 
-void Comandos::apagaArquivoComNomeTabela() {
-    cout << "Apagando tabela " << Comandos::keyword << "\n";
-}
+void Comandos::resumoDaTabela(string tabela) {}
+
+void Comandos::listarTabelas() {}
+
+void Comandos::inserirRegistro(string tabela, string registro) {}
+
+void Comandos::buscaEmTabela(string modifier, string tabela, string busca) {}
+
+void Comandos::apresentarRegistrosUltimaBusca(string tabela) {}
+
+void Comandos::removeRegistrosUltimaBusca(string tabela) {}
+
+void Comandos::criaIndice(string modifier, string tabela, string chave) {}
+
+void Comandos::removeIndiceChave(string tabela, string chave) {}
+
+void Comandos::geraNovoIndiceDeTabelaChave(string tabela, string chave) {}
