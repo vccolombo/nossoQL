d09ec0089ccdf96a365ad4f838ef38c616bec216
marcogrivol <mbgrivol@gmail.com>
2019-08-23 15:57:44 -0300

    formatacao de codigo

--- Comandos.cpp
+++ Comandos.cpp
@@ -75,7 +75,6 @@ void Comandos::geraNovoIndiceDeTabelaChave(string tabela, string chave) {
 // remove a palavra antes do delimitador da string input
 string Comandos::retornaPalavraDeInput (string &input, char delimitador, bool removerEspacosAfrente) {
   string palavra;
-
   if (removerEspacosAfrente) {
     int i = 0;
     while (input[i] == ' ')
@@ -94,13 +93,11 @@ string Comandos::retornaPalavraDeInput (string &input, char delimitador, bool re
 
 //separa o tipo e o nome de cada um dos campos da tabela
 string* Comandos::parseCampoCT(string input) {
-  
   int i = 0, quant_campo = 0;
-  int ponto_virgula =0, dois_pontos = 0;
-  int idx_ini = 0,idx_fim;
+  int ponto_virgula = 0, dois_pontos = 0;
+  int idx_ini = 0, idx_fim;
   string palavra, *campo;
   input.erase(0, 1);
-
   //varredura para encontrar problemas na string como
   //Existencia de ' ' e separção incorreta de campos 
   //contagem de campos
@@ -118,18 +115,16 @@ string* Comandos::parseCampoCT(string input) {
   if (dois_pontos != ponto_virgula || dois_pontos == 0){
     return NULL;
   }
-  campo = new string[1+quant_campo*2];
+  campo = new string[1 + quant_campo * 2];
   campo[0] = quant_campo;
-  i=1;
+  i = 1;
 
   //separação de TIPO e NOME de cada campo
-  for(int j=0;j<quant_campo;j++){
-
-    idx_fim = input.find(':',idx_ini);
-    palavra = input.substr(idx_ini, idx_fim-idx_ini);
+  for(int j = 0; j < quant_campo; j++){
+    idx_fim = input.find(':', idx_ini);
+    palavra = input.substr(idx_ini, idx_fim - idx_ini);
     transform(palavra.begin(), palavra.end(), palavra.begin(), ::toupper);
-
-    
+  
     if (palavra.compare("INT") == 0 || palavra.compare("STR") == 0)
       campo[i++] = palavra;
     else if (palavra.compare("FLT") == 0 || palavra.compare("BIN") == 0)
@@ -139,27 +134,23 @@ string* Comandos::parseCampoCT(string input) {
       return NULL;
     }
 
-
-    idx_ini = idx_fim+1;
-    idx_fim = input.find(';',idx_ini);
-    palavra = input.substr(idx_ini, idx_fim-idx_ini);
+    idx_ini = idx_fim + 1;
+    idx_fim = input.find(';', idx_ini);
+    palavra = input.substr(idx_ini, idx_fim - idx_ini);
     if (strlen(palavra.c_str()) == 0)
       return NULL;
     campo[i++] = palavra;
-    idx_ini = idx_fim+1;
+    idx_ini = idx_fim + 1;
   }
   return campo;
 }
 
-
-
 void Comandos::parseInsercao(string registro) {
-  std::cout << "Registros a serem inseridos: " << '\n';
+  cout << "Registros a serem inseridos: " << '\n';
   int campo = 0;
   while (registro.length() > 0) {
-    std::cout << "Campo " << ++campo << ": ";
-    std::cout << retornaPalavraDeInput(registro, ';') << '\n';
+    cout << "Campo " << ++campo << ": ";
+    cout << retornaPalavraDeInput(registro, ';') << '\n';
   }
-  std::cout << '\n';
-
+  cout << '\n';
 }
